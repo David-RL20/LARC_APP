d633055fb0c2d96a1ba75415cff3790ab3008829
David Rodriguez <alejandro20rodriguez04@gmail.com>
2020-09-29 18:18:29 -0700

    agregar iconos

--- src/sections/device/ControlDevice.js
+++ src/sections/device/ControlDevice.js
@@ -1,2 +1,24 @@
 // Este componente contiene la seleccion del canal output y el control de abrir, cerrar, por tiempo y por llamada.
 //los controles anteriormente explicados solo controlaran el dispositivo seleccionado con su canal seleccionado.
+import React from 'react';
+import {View, Image, Text, TouchableOpacity} from 'react-native';
+
+function Controll() {
+  return (
+    <View style={style.container}>
+      <View>CHANNE:</View>
+      <View style={style.device}>
+        <Text style={style.nameDevice}> Device </Text>
+        <Text style={style.numberDevice}> 66645623515 </Text>
+        <View>
+          <TouchableOpacity>1</TouchableOpacity>
+          <TouchableOpacity>2</TouchableOpacity>
+          <TouchableOpacity>3</TouchableOpacity>
+          <TouchableOpacity>4</TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+export default Controll;
